2411a2b9df725be88cd2c7e72b81b14228d3f6af
Agustinus <lagustinus211@gmail.com>
2023-06-05 09:54:22 +0700

    pass keluhan

---
+++ resources/views/Compone/Keluhan.blade.php
@@ -0,0 +1,18 @@
+  <!-- Team Start -->
+  <div class="container-xxl py-5">
+    <div class="container">
+        <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+            <p class="fs-5 fw-medium text-primary">Pelaporan Keluhan</p>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <iframe src="{{setting('site.keluhan')}}" frameborder="0" marginheight="0" marginwidth="0" class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative col-12" style="height: 80vh">
+                    Memuat…
+                </iframe>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Team End -->
+
+
